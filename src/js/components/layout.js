fa6a023f72a855ebe5e9066119437ac4ef18592a
Mark Arciaga <mark.arciaga@gmail.com>
2017-01-28 10:55:30 -0800

    Add socket.io client library and basic sample connection

--- src/js/components/layout.js
+++ src/js/components/layout.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import io from 'socket.io-client';
+
+// for demo purposes
+// socket.io polls so if there's no connection, you'll get errors
+const socket = io('http://localhost:3000');
+socket.on('now-playing', obj => console.log(obj));
 
 const Layout = () => <h1>Hello World</h1>;
 
