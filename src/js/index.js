c1dd2ab57aaeb22c842dddd93b4db34b0c616ad5
Mark Arciaga <mark.arciaga@gmail.com>
2017-04-12 07:23:02 -0700

    Setup of React-router 4 with basic example of route config. Fix webpack dev server to work with reloading on all URLs

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-import createBrowserHistory from 'history/createBrowserHistory'
 import { Provider } from 'react-redux';
 import storeFactory from './configureStore';
-import routes from './routes';
+import Layout from './components/layout';
+import createBrowserHistory from 'history/createBrowserHistory'
 
-
+const history = createBrowserHistory();
 const initialState = {};
 const store = storeFactory(initialState);
-const history = createBrowserHistory();
 
 render(
-    <Provider store={store}>
-        <Router history={history} routes={routes} />
+    <Provider store={store} history={history}>
+        <Layout />
     </Provider>,
     document.getElementById('app')
 );
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,10 +1,24 @@
-import Layout from './components/layout';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import About from './components/about';
+
+// wrap <Route> and use this everywhere instead, then when
+// sub routes are added to any route it'll work
+const RouteWithSubRoutes = (route) => (
+    <Route
+        path={route.path}
+        render={props => (
+            // pass the sub-routes down to keep nesting
+            <route.component {...props} routes={route.routes}/>
+        )}
+    />
+);
 
 const routes = [
     {
-        path: '/',
-        component: Layout
+        path: '/about',
+        component: About
     }
 ];
 
-export default routes;
+export { routes, RouteWithSubRoutes };
